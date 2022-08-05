3e7c7942ed0a1189836e46ceb478f2aa3e459f8f
David R <davidrubio97@outlook.com>
2022-08-05 08:42:12 +0200

    update code

---
+++ src/StyleSheet.py
@@ -0,0 +1,16 @@
+style_sheet = """
+    QPushButton{
+        color: #4E4C4C;
+        font: 14px 'Helvetica';
+        background-color: #ebeced  
+    }
+    QPushButton:hover{
+        
+    }
+    QLineEdit{
+        border-radius: 5px;
+    }
+    QLineEdit:focus{
+        border: 2px solid  #a9d1f4
+    }
+"""
