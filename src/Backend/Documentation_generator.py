10abe503d207e90a049e48d6e003d5b76bd65c0b
David R <davidrubio97@outlook.com>
2023-09-15 00:02:43 +0200

    update pdf methods

--- src/Backend/Documentation_generator.py
+++ src/Backend/Documentation_generator.py
@@ -8,6 +8,7 @@ class Documentation_generator():
     """
     def __init__(self):
         self.read_config()
+        self.pages = 0
 
 
     def read_config(self):
@@ -37,6 +38,7 @@ class Documentation_generator():
         self.ports, self.generics, self.comments, _ = hdl.init()
         if self.source_code:
             self.file = open(file_input, "r")
+            self.file = self.file.readlines()
 
         self.gen_pdf(file_output)
         
@@ -48,14 +50,8 @@ class Documentation_generator():
             - file_output: this is the name of the pdf
         """
         self.pdf = FPDF()
-        self.pdf.add_page()
-        self.pdf.set_draw_color(0, 0, 0)
-        self.pdf.line(10, 20, 200, 20)
-        self.pdf.line(10, 277, 200, 277)
-        try:
-            self.pdf.cell(0, 0, link=self.pdf.image('../img/logo/prueba.png', 190, 10, 7))
-        except:
-            pass
+        self.add_page()
+        
 
         self.pdf_text(20, 35, file_output[:-4], size=30)
         base_x = 31
@@ -64,9 +60,87 @@ class Documentation_generator():
         base_y = self.pdf_generic(base_x, base_y)
         base_y = self.pdf_port(base_x, base_y)
 
+        self.insert_code(base_y)
+
         self.pdf.output(file_output, 'F')
 
 
+    def add_page(self):
+        """
+        This method adds a page with logo, lines and number
+        """
+        try:
+            self.pdf.cell(0, 0, link=self.pdf.image('./img/logo/prueba.png', 190, 10, 7))
+        except:
+            pass
+        self.pdf.add_page()
+        self.pdf.set_draw_color(0, 0, 0)
+        self.pdf.line(10, 20, 200, 20)
+        self.pdf.line(10, 277, 200, 277)
+        self.pages += 1
+        self.pdf_text(190, 285, str(self.pages), size=13)
+        self.pdf_text(20, 285, "Created by HDLHelper")
+
+
+    def insert_code(self, y):
+        """
+        This method adds source code in pdf
+        Input:
+            - y: y position
+        """
+        if self.source_code == "True":
+            self.pdf_text(11, y, "Source", underline=True, italic=True, color=(255,0,0), size=15)
+            y=y+5
+            self.insert_rect(20, y, 190, 273, fill=True, fill_color=(255, 215, 125))
+            cont = 5
+            cont_lines = 0
+            lon_max = len(self.file)
+            for line in self.file:
+                cont_lines += 1
+                line = line.replace("\t", "    ")
+                self.pdf_text(25, y+cont, line, color=(0,0,255))
+                cont += 5
+                if y+cont > 270:
+                    self.add_page()
+                    if lon_max-cont_lines < 48:
+                        self.insert_rect(20, 24, 190, 24+(lon_max-cont_lines)*5, fill=True, fill_color=(255, 215, 125))
+                    else:
+                        self.insert_rect(20, 24, 190, 273, fill=True, fill_color=(255, 215, 125))
+
+                    y = 24
+                    cont=5
+            
+
+    def insert_rect(self, x1, y1, x2, y2, color=(0,0,0), fill=False, fill_color=(0,0,0)):
+        """
+        This method inserts a retangle
+        Input:
+            - x1: first x position
+            - y1: first y position
+            - x2: second x position
+            - y2: second y position
+            - color: tuple with border color
+            - fill: flag to fill rectangle
+            - fill_color: tuple with the filled color
+        """
+        if x1 < x2:
+            w = x2-x1
+        else:
+            w = x1-x2
+
+        if y1 < y2:
+            h = y2-y1
+        else:
+            h = y1-y2
+        
+        self.pdf.set_draw_color(color[0], color[1], color[2])
+        self.pdf.rect(x1, y1, w, h)
+
+        if fill:
+            self.pdf.set_fill_color(fill_color[0], fill_color[1], fill_color[2])
+            self.pdf.rect(x1, y1, w, h, 'F')
+
+        
 
     def pdf_generic(self, base_x, base_y):
         """
@@ -83,13 +157,11 @@ class Documentation_generator():
             for generic in self.generics:
                 if type(generic[0]) is list:
                     for i in generic[0]:
-                        self.pdf_text(base_x, base_y+cont, '- '+i)
-                        cont += 5
+                        cont += self.generic_desciption(base_x, base_y+cont, generic, i)
                 else:
-                    self.pdf_text(base_x, base_y+cont, '- '+generic[0])
-                    cont += 5
+                    cont += self.generic_desciption(base_x, base_y+cont, generic)
 
-            base_y= base_y+cont
+            base_y= base_y+cont+5
         return base_y
 
 
@@ -111,7 +183,7 @@ class Documentation_generator():
                         cont += self.port_desciption(base_x, base_y+cont, port, i)
                 else:
                     cont += self.port_desciption(base_x, base_y+cont, port)
-            base_y = base_y+cont
+            base_y = base_y+cont+5
         return base_y
 
 
@@ -155,11 +227,41 @@ class Documentation_generator():
 
         for com in self.comments:
             if com[1] == port[3]:
-                self.pdf_text(x+40, y+5, ": " + com[0], color=(0,0,0))
+                self.pdf_text(x+40, y+5, ": " + com[0], color=(0, 111, 1))
                 cont = 10
         
         return cont
         
+    def generic_desciption(self, x, y, generic, generic_=""):
+        """
+        This method adds the description of the ports
+        Input:
+            - x: x position
+            - y: y position
+            - port: list with the port
+            - port_: name of the port if it is individual
+        Return:
+            - cont: increment of lines after write one port
+        """
+        cont = 5
+        if generic_:
+            self.pdf_text(x, y, '- '+generic_)
+        else:
+            self.pdf_text(x, y, '- '+generic[0])
+
+        text_type = " <type> " + generic[1] 
+        self.pdf_text(x+40, y, text_type, color=(0,0,255))
+
+        value_text = " <value> " + generic[2]
+
+        self.pdf_text(x+75, y, value_text, color=(255,0,0))
+
+        for com in self.comments:
+            if com[1] == generic[3]:
+                self.pdf_text(x+40, y+5, ": " + com[0], color=(0, 111, 1))
+                cont = 10
+        
+        return cont
 
 
     def pdf_text(self, x, y, text, underline=False, bold=False, italic=False, color=(0,0,0), size=10):
