1e315a28af0c96d61c3d05b850f6219eb1121bef
David R <davidrubio97@outlook.com>
2024-03-05 22:34:33 +0100

    update window

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from GUI.HDLHelper_gui import HDLHelper_gui
 import sys
-from PyQt5.QtWidgets import QApplication, QSplashScreen
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtGui import QPixmap
+from PySide6.QtWidgets import QApplication, QSplashScreen
+from PySide6 import QtGui, QtCore
+from PySide6.QtGui import QPixmap
 
 """ This code is the start point of the program
 
@@ -12,4 +12,4 @@ app = QApplication(sys.argv)    # call Qt application class
 app.setWindowIcon(QtGui.QIcon('icon.ico'))
 hdlhelper_gui = HDLHelper_gui() # call HDLHelper application
 hdlhelper_gui.show()            # show the interface
-sys.exit(app.exec_())           # exit at the finish
+sys.exit(app.exec())           # exit at the finish
